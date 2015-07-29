3042ff0c86fcdaad5c573de86a8c323344837bb3
Russell Dempsey <SgtPooki@gmail.com>
2015-07-28 20:24:09 -0500

    Replace compiler.jar in repo with bower dep

--- tasks/js1k.js
+++ tasks/js1k.js
@@ -10,23 +10,23 @@ var closureCompiler = require('gulp-closure-compiler');
 module.exports = {
     dependencies: [],
     task: function (gulp) {
-		return gulp.src('src/js/index.js')
-			.pipe(bytediff.start())
-			.pipe(bytediff.stop())
+        return gulp.src('src/js/index.js')
+            .pipe(bytediff.start())
+            .pipe(bytediff.stop())
             .pipe(closureCompiler({
-                compilerPath: 'compiler.jar',
+                compilerPath: 'bower_components/closure-compiler/compiler.jar',
                 compilation_level: 'ADVANCED_OPTIMIZATIONS',
                 fileName: 'index.js'
             }))
-			.pipe(bytediff.start())
-	        .pipe(jscrush())
-			.pipe(bytediff.stop())
-			.pipe(micro({limit: 1024})).
-				on('error', function (error) {
-					console.log(error.message);
-					this.emit('end');
-					return error.message;
-				})
-	        .pipe(gulp.dest('dist'));
+            .pipe(bytediff.start())
+            .pipe(jscrush())
+            .pipe(bytediff.stop())
+            .pipe(micro({limit: 1024})).
+                on('error', function (error) {
+                    console.log(error.message);
+                    this.emit('end');
+                    return error.message;
+                })
+            .pipe(gulp.dest('dist'));
     }
 };
